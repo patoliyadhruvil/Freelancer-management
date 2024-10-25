1c2cdb021e2b307726fa9cb9c6e81f266ba73a16
dhruvilpatoliya <dhruvilpatoliya3@gmail.com>
2024-10-26 00:11:42 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="foo">
+            <p>
+                Created by <a href="https://github.com/patoliyadhruvil?tab=repositories" target="_blank" rel="noopener noreferrer">Dhruvil⭐</a>
+            </p>
+        </footer>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { store } from './redux/app/store';
+import { Provider } from 'react-redux';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
+    </Provider>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
